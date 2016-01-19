425973637424ff9eaa73dcc635239e24f59977f1
vatanambib <hackervatanbib@gmail.com>
2016-01-19 20:37:38 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {119989724},
+    sudo_users = {118333567},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
